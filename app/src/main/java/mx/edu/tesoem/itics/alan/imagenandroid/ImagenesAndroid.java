fe65fd810024f5ae388ac672cd24a9b60805ee6a
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 10:20:21 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/alan/imagenandroid/ImagenesAndroid.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.itics.alan.imagenandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagenesAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagenes_android);
+    }
+}
